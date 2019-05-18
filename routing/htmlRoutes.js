532d4b0faaf702763fa73a2e6b2b1a6a48b09a01
apizarras <angelalee.p@gmail.com>
2019-05-18 12:37:22 -0500

    trying to create routes

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -9,11 +9,17 @@ server.listen(PORT, function() {
 });
 
 function handleRequest(request, response) {
-    const paht = req.url;
+    const path = req.url;
     switch (path) {
         case "/home":
             return displayRoot(path, request, response);
         case "/survey":
             return displaySurvery(path, request, response);
-    }
-}
+    };
+};
+
+function displayRoot(url, request, response) {
+    const home = ../public/home.html;
+    response.writeHead(200, {"Content-Type": "text/html});
+    response.end(home);
+};

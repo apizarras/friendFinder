980e7fe2715fb734f491a5717c65cac09e338ab0
apizarras <angelalee.p@gmail.com>
2019-05-22 13:53:28 -0500

    set up routing

---
+++ routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+modules.export = function(app) {
+    app.get("/", function(request, response) {
+        response.sendFile(path.join(__dirname, "./public/home.html"));
+    });
+    
+    app.get("/survey", function (request, response) {
+        response.sendFile(path.join(__dirname, "./public/survey.html"));
+    });
+}
+
--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 const express = require("express");
-const path = require("path");
 const http = require("http");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// function handleRequest(request, response) {
-//     const path = request.url;
-//     switch (path) {
-//         case "/home":
-//             return displayRoot(path, request, response);
-//         case "/survey":
-//             return displaySurvery(path, request, response);
-//     };
-// };
-
-app.get("/", function(request, response) {
-    response.sendFile(path.join(__dirname, "./public/home.html"));
-});
+require("./routing/apiRoutes.js");
+require("./routing/htmlRoutes.js");
 
 app.listen(PORT, function() {
     console.log("app listening at PORT: "+ PORT);

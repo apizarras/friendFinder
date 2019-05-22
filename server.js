de1a378668aa9bffca6fa42a558ee31018291c34
apizarras <angelalee.p@gmail.com>
2019-05-21 21:46:20 -0500

    adjust for express

--- server.js
+++ server.js
@@ -2,20 +2,26 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 
+const app = express();
 const PORT = 4000;
 
-const server = http.createServer(handleRequest);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-server.listen(PORT, function() {
-    console.log("listening on port: "+ PORT);
+// function handleRequest(request, response) {
+//     const path = request.url;
+//     switch (path) {
+//         case "/home":
+//             return displayRoot(path, request, response);
+//         case "/survey":
+//             return displaySurvery(path, request, response);
+//     };
+// };
+
+app.get("/", function(request, response) {
+    response.sendFile(path.join(__dirname, "./public/home.html"));
 });
 
-function handleRequest(request, response) {
-    const path = req.url;
-    switch (path) {
-        case "/home":
-            return displayRoot(path, request, response);
-        case "/survey":
-            return displaySurvery(path, request, response);
-    };
-};
+app.listen(PORT, function() {
+    console.log("app listening at PORT: "+ PORT);
+});

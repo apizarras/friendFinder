d2ce2e7ee36ff353cec51d1408855f206d29d3a1
apizarras <angelalee.p@gmail.com>
2019-05-24 17:02:18 -0500

    adjust modal image size

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
 {
     "name":"Fred",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://images.unsplash.com/photo-1542141372-98a047557466?ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
     "scores":[
         5,
         1,
